216bc65c3bdc6f8dc2f8ddf3198fabe5117bc290
Joshua <jdkelly2015@gmail.com>
2022-01-18 17:32:31 -0500

    created characters object. pseudo coded game functions

---
+++ assets/javascript/script.js
@@ -0,0 +1,54 @@
+// Character Object
+const characters = [
+  {
+    id: 1,
+    name: "Obi-Wan Kenobi",
+    hp: 150,
+    ap: 8,
+    cap: 12,
+  },
+  {
+    id: 2,
+    name: "Anakin Skywalker",
+    hp: 175,
+    ap: 10,
+    cap: 10,
+  },
+  {
+    id: 3,
+    name: "Darth Maul",
+    hp: 125,
+    ap: 4,
+    cap: 15,
+  },
+  {
+    id: 4,
+    name: "Darth Sidious",
+    hp: 100,
+    ap: 5,
+    cap: 10,
+  },
+];
+// Variables for DOM manipulation
+
+// Select Character
+const selectCharacter = () => {
+  // allow character cards to be clickable
+  // if no player is selected, then fill player slot
+  // if no opponent is selected, fill opponent slot
+};
+
+// arena
+const attack = () => {
+  // player clicks attack button which makes the player ap subtract from villain hp
+  // each attack will add its own ap to itself(8+8+8+8), villain cap stays the same
+  // if either player or vilain hp is brought to 0 or less than 0, game is over
+  // if player hp reachs 0, player loses
+  // if villain hp reachs 0, villain card is moved to graveyard(cards in graveyard are not clickable)
+  // another villain must be selected
+};
+// restart game
+const restartGame = () => {
+  // all stats are reset back to original
+  // all cards are put back in character select
+};
